c20a912647fa4fc45ed7906154f21c0714b870e3
Brodrick Favour <Brodrickfavour0@gmail.com>
2025-07-28 20:20:34 +0100

    updated api.js

--- ecogasafrica/frontend/src/api.js
+++ ecogasafrica/frontend/src/api.js
@@ -17,17 +17,4 @@ const api = axios.create({
 });
 
 // Add a request interceptor to include the JWT token
-api.interceptors.request.use(
-  (config) => {
-    const user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.token) {
-      config.headers.Authorization = `Bearer ${user.token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
-export default api;
