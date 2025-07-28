6cc086b63445df6e27a431b8bcc6306e01f4b162
Brodrick Favour <Brodrickfavour0@gmail.com>
2025-07-28 15:49:37 +0100

    make some changes on api.js

--- ecogasafrica/frontend/src/api.js
+++ ecogasafrica/frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api', // Adjust base URL for deployment
+  baseURL: import.meta.env.VITE_API_URL || 'https://backend-production-adb6.up.railway.app', // Adjust base URL for deployment
 });
 
 // Add a request interceptor to include the JWT token
